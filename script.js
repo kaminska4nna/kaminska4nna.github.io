69e582c5174904a2393cebe0c83a0851bd14e800
kaminska4nna <kaminska4nna@users.noreply.github.com>
2018-01-25 00:35:02 +0100

    rep3

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://morning-springs-79162.herokuapp.com/v1/task/';
+  const apiRoot = 'https://morning-springs-79162.herokuapp.com/v1/task/';
   const trelloApiRoot = 'http://localhost:8080/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
